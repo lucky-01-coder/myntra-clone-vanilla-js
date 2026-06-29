c18f3f4d80efb3ff5fe461c000ed080bf94c9a93
lucky Ghatak <luckare1401@gmail.com>
2026-06-29 15:34:10 +0530

    Initial commit: Myntra Clone using HTML, CSS & JavaScript

---
+++ scripts/index.js
@@ -0,0 +1,49 @@
+let bagItems = [];
+onLoad();
+
+function onLoad() {
+  let bagItemsStr = localStorage.getItem("bagItems");
+  bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+  displayItemsOnHomePage();
+  displayBagIcon();
+}
+
+function addToBag(itemID) {
+  bagItems.push(itemID);
+  localStorage.setItem("bagItems", JSON.stringify(bagItems));
+  displayBagIcon();
+}
+
+function displayBagIcon() {
+  let bagItemCount = document.querySelector(".bag-item-count");
+  if (bagItems.length > 0) {
+    bagItemCount.style.visibility = "visible";
+    bagItemCount.innerText = bagItems.length;
+  } else {
+    bagItemCount.style.visibility = "hidden";
+  }
+}
+
+function displayItemsOnHomePage() {
+  let itemsContainer = document.querySelector(".items-container");
+  if (!itemsContainer) {
+    return;
+  }
+  let innerHtml = "";
+  items.forEach((item) => {
+    innerHtml += `
+  <div class="item-container">
+          <img class="item-image" src="${item.image}" alt="item image" />
+          <div class="rating">${item.rating.stars} ⭐ | ${item.rating.count}</div>
+          <div class="company-name">${item.company}</div>
+          <div class="item-name">${item.item_name}</div>
+          <div class="price">
+            <span class="current-price">Rs ${item.current_price}</span
+            ><span class="original-price">Rs ${item.original_price}</span
+            ><span class="discount">(${item.discount_percentage}% OFF)</span>
+          </div>
+          <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+        </div>`;
+  });
+  itemsContainer.innerHTML = innerHtml;
+}
